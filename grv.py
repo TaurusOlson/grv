dc3edba875884f9433503c25febbd0f8ce9ebce6
Taurus Olson <taurusolson@gmail.com>
2013-05-14 01:50:02 +0200

    Use argparse and add normal distribution

--- grv.py
+++ grv.py
@@ -14,28 +14,66 @@ So far only uniform distribution is supported.
 import random
 import sys
 import os
+import argparse
 
+def uniform(domain=None):
+    """Generate a random value in the given domain using a uniform distribution
 
-def generate_random(start=0, stop=1):
-    """Generate a random value between start and stop
+    Usage
+    -----
 
-    r = generate_random(start, stop)
+    r = uniform(domain)
+    r.next()
 
     Parameters
     ----------
-    start : int
-        The lower bound of the range (optional)
+    domain : list
+        The domain on which the values are taken
 
-    stop : int
-        The upper bound of the range (optional)
+    Returns
+    -------
+    A generator returning a random value within the chosen domain
+
+    """
+    if domain:
+        start = domain[0]
+        stop = domain[1]
+    else:
+        start = 0
+        stop = 1
+
+    while True:
+        yield random.random() * (stop - start) + start
+
+
+def normal(parameters=None):
+    """Generate a random value in the given domain using a normal distribution
+
+    Usage
+    -----
+
+    r = normal(parameters)
+    r.next()
+
+    Parameters
+    ----------
+    domain : list
+        The domain on which the values are taken
 
     Returns
     -------
     A generator returning a random value between start and stop
 
     """
+    if parameters:
+        mu = parameters[0]
+        sigma = parameters[1]
+    else:
+        mu = 0
+        sigma = 1
+
     while True:
-        yield random.random() * (stop - start) + start
+        yield random.gauss(mu, sigma)
 
 
 def usage():
@@ -54,26 +92,22 @@ Usage:
     %s <min> <max> <n>""" % ((script, ) * 4)
 
 
-n = 1
+# The argument parser
+parser = argparse.ArgumentParser(description="Generate random values")
+parser.add_argument("parameters", type=float, nargs="*", help="a distribution")
+parser.add_argument("--uniform", dest="accumulate", action="store_const",
+                    const=uniform, default=uniform)
+parser.add_argument("--normal", dest="accumulate", action="store_const",
+                    const=normal, default=normal)
 
-# Usage: generate_random
-if len(sys.argv) == 1:
-    r = generate_random()
-
-# Usage: generate_random <start> <stop>
-elif 3 <= len(sys.argv) < 5:
-    start = float(sys.argv[1])
-    stop = float(sys.argv[2])
-    r = generate_random(start, stop)
-
-    # Usage: generate_random <start> <stop> <n>
-    if len(sys.argv) == 4:
-        n = int(sys.argv[3])
-else:
-    usage()
-    sys.exit()
+args = parser.parse_args()
 
 
 if __name__ == '__main__':
+    n = 1
+    r = args.accumulate(args.parameters)
+    if len(args.parameters) == 3:
+        n = int(args.parameters[2])
+
     for i in range(n):
         print r.next()
